05b1ded581ca88cdb67c40ceb465957e84e15dac
Dmitry_Zildermanis <Uran.Deid@gmail.com>
2025-05-11 21:59:43 +0400

    add title function and docstring capital_letter_finction

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def capital_letter_function(text):
+    ```функция принимает на вход строку и возвращает еe со всеми заглавными буквами```
     return text.upper()
+
+def title_letter_function(text):
+    ```принимает на вход строку и возвращает ее с заглавными буквами в начале каждого слова```
+    return text.title()
